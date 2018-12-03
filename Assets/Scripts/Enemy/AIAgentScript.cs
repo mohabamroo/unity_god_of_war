42e659e59c3c21bf8be050ec314489e3ec6e4da4
Bavly's <bavlye.shenouda95@gmail.com>
2018-12-03 14:03:13 +0200

    Approching Enemy Prefab

--- Assets/Scripts/Enemy/AIAgentScript.cs
+++ Assets/Scripts/Enemy/AIAgentScript.cs
@@ -1,16 +1,88 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AIAgentScript : MonoBehaviour {
+public class AIAgentScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Animator animator;
+    NavMeshAgent nav;
+    Transform player;
+
+    public int health;
+    float time;
+
+    // Use this for initialization
+    void Start()
+    {
+        // animator.SetBool("run", false);
+        // playerHealth = player.GetComponent <PlayerHealth> ();
+        // enemyHealth = GetComponent <EnemyHealth> ();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Enemy always following the player
+        nav.SetDestination(player.position);
+
+        if (health <= 0)
+        {
+            time += Time.deltaTime;
+            // Trigger dead animation
+            animator.SetBool("dead", true);
+            // Delay then destroy the object
+            if (time > 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Player is within enemy attack zone, trigger attack animation
+        if (col.gameObject.tag == "Player")
+        {
+            animator.SetBool("attack", true);
+            animator.SetBool("run", false);
+            animator.SetBool("hit", false);
+            animator.SetBool("dead", false);
+        }
+
+        // Player attacked enemy, decrement HP and trigger the hit animation
+        if (col.gameObject.tag == "Weapon")
+        {
+            animator.SetBool("attack", false);
+            animator.SetBool("run", false);
+            animator.SetBool("hit", true);
+            animator.SetBool("dead", false);
+            // Decrement HP
+            health -= 10;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        // Enemy is not within attack zone, enable running animation
+        if (col.gameObject.tag == "Player")
+        {
+            animator.SetBool("run", true);
+            animator.SetBool("attack", false);
+            animator.SetBool("hit", false);
+            animator.SetBool("dead", false);
+        }
+
+        if (col.gameObject.tag == "Weapon")
+        {
+            animator.SetBool("attack", false);
+            animator.SetBool("run", true);
+            animator.SetBool("hit", false);
+            animator.SetBool("dead", false);
+        }
+    }
 }
