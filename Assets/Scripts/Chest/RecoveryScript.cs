18ad1422cd6bf2b16233dd66c405da9050ac32c8
MOHAB <mohabamr1@gmail.com>
2018-12-05 21:07:07 +0200

    basics working

--- Assets/Scripts/Chest/RecoveryScript.cs
+++ Assets/Scripts/Chest/RecoveryScript.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RecoveryScript : MonoBehaviour {
+public class RecoveryScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int chestHP;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        // Player in chest proximity and pressed E
+        if (col.gameObject.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+            // call a function in the player class that maximizes/increases his HP
+        }
+    }
 }
