93a08519318951cd5579da9a316fd7e55cf62809
mekladious <miraekladious@gmail.com>
2018-12-08 17:44:06 +0200

    merge with mohab

---
+++ Assets/Scripts/Enemy/AIEnemy2Script.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIEnemy2Script : MonoBehaviour
+{
+
+    public Animator animator;
+    public GameObject skeleton;
+    NavMeshAgent nav;
+    Transform player;
+
+    public int health;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Falsify();
+        //startPosition = transform.localPosition;
+        // Enemy always following the player
+        nav.SetDestination(player.position);
+        // Stopping distance for the enemy to throw fireballs
+        nav.stoppingDistance = GetComponent<enemy2Script>().maxDistance;
+
+        Debug.Log(animator.GetBool("dead"));
+
+        if (health <= 0 && !animator.GetBool("dead"))
+        {
+            // Trigger dead animation;
+            Dead();
+            return;
+        }
+
+        transform.LookAt(player);
+        if (nav.remainingDistance<=GetComponent<enemy2Script>().maxDistance)
+            Attack();
+        else
+            Run();
+    }
+
+    void OnCollisionStay(Collision col)
+    { 
+        // Player attacked enemy, decrement HP and trigger the hit animation
+        if (col.gameObject.tag == "Weapon")
+        {
+            // Decrement HP
+            health -= 10;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Weapon")
+            Falsify();
+    }
+
+    void Falsify()
+    {
+        animator.SetBool("attack", false);
+        animator.SetBool("run", false);
+        animator.SetBool("hit", false);
+        animator.SetBool("dead", false);
+    }
+
+    void Attack()
+    {
+        Falsify();
+        animator.SetBool("attack", true);
+    }
+
+    void Run()
+    {
+        Falsify();
+        animator.SetBool("run", true);
+    }
+
+    void Hit()
+    {
+        Falsify();
+        animator.SetBool("hit", true);
+    }
+
+    void Dead()
+    {
+        Falsify();
+        animator.SetBool("dead", true);
+        // Destroy the object after 2.0f s delay
+        Destroy(gameObject, 2.0f);
+    }
+
+}
---
+++ Assets/Scripts/Enemy/ProjectileScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+    public ParticleSystem explosion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(transform.gameObject);
+        ParticleSystem exp = Instantiate(explosion, collision.contacts[0].point, collision.gameObject.transform.rotation);
+        Destroy(exp.gameObject, 1.0f);
+
+        if(collision.gameObject.tag == "Player")
+        {
+
+        }
+    }
+}
