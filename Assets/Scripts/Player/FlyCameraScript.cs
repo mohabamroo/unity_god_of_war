6b16b9b44369eec62f019c76ba2ea7973eb89c56
A. Elmeniawy <ameniawy18@gmail.com>
2018-12-14 19:44:08 +0200

    commented camera

--- Assets/Scripts/Player/FlyCameraScript.cs
+++ Assets/Scripts/Player/FlyCameraScript.cs
@@ -28,11 +28,11 @@ public class FlyCameraScript : MonoBehaviour
 
     void Update()
     {
-        lastMouse = Input.mousePosition - lastMouse;
-        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
-        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-        transform.eulerAngles = lastMouse;
-        lastMouse = Input.mousePosition;
+        // lastMouse = Input.mousePosition - lastMouse;
+        // lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+        // lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        // transform.eulerAngles = lastMouse;
+        // lastMouse = Input.mousePosition;
         // Mouse  camera angle done. 
 
         // transform.LookAt(this.player);
